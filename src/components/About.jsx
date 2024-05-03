9c2e7eb8ddc8fd6b6886df26adde92f58a0c0d60
Vraj Patel <31vrajpatel@gmail.com>
2024-05-03 23:39:25 +0530

    contact form

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,112 +1,176 @@
-import React from 'react'
-import { FaGithub, FaLinkedin } from 'react-icons/fa'
-import { FaSquareXTwitter, FaReact } from 'react-icons/fa6'
-import { SiGeeksforgeeks, SiLeetcode } from 'react-icons/si'
-import {  C, Mongo, Python, Vscode, JS, Cpp, Css, Express, Html, Java, Node, ReactLogo  } from '../assets/technologies_logo/index'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { FaSquareXTwitter, FaReact } from "react-icons/fa6";
+import { SiGeeksforgeeks, SiLeetcode } from "react-icons/si";
+import {
+    C,
+    Mongo,
+    Python,
+    Vscode,
+    JS,
+    Cpp,
+    Css,
+    Express,
+    Html,
+    Java,
+    Node,
+    ReactLogo,
+} from "../assets/technologies_logo/index";
+import { Link } from "react-router-dom";
 
 const About = () => {
-  return (
-    <div className="container mx-auto w-full  mt-5 ">
+    return (
+        <div className="container mx-auto w-full  mt-5 ">
             <div className="text-xl md:text-3xl text-center m-2 font-semibold">
                 About
             </div>
             <div className="w-full container mx-auto flex justify-center items-center flex-wrap-reverse mt-5">
-            <div className="left-container md:w-1/2 w-96 p-5 ">
-                <div className='technologies-container'>
-                  <div className='poppins-bold text-2xl text-center'>Technologies</div>
-                  <div className='flex justify-center p-5 items-center flex-wrap  mt-5 gap-4'>
-                    
-                   
-                    <div >
-                      <img className='logo  w-24 h-24  rounded-xl object-cover ' src={Html} alt="" />
+                <div className="left-container md:w-1/2 w-96 p-5 ">
+                    <div className="technologies-container">
+                        <div className="poppins-bold text-2xl text-center">
+                            Technologies
+                        </div>
+                        <div className="flex justify-center p-5 items-center flex-wrap  mt-5 gap-4">
+                            <div>
+                                <img
+                                    className="logo  w-24 h-24  rounded-xl object-cover "
+                                    src={Html}
+                                    alt=""
+                                />
+                            </div>
+                            <div>
+                                <img
+                                    className="logo  w-24 h-24  rounded-xl object-cover "
+                                    src={Css}
+                                    alt=""
+                                />
+                            </div>
+                            <div>
+                                <img
+                                    className="logo  w-24 h-24  rounded-xl object-cover "
+                                    src={JS}
+                                    alt=""
+                                />
+                            </div>
+                            <div>
+                                <img
+                                    className="logo  w-24 h-24  rounded-xl object-cover "
+                                    src={ReactLogo}
+                                    alt=""
+                                />
+                            </div>
+                            <div>
+                                <img
+                                    className="logo  w-24 h-24  rounded-xl object-cover "
+                                    src={Node}
+                                    alt=""
+                                />
+                            </div>
+                            <div>
+                                <img
+                                    className="logo  w-24 h-24  rounded-xl object-cover "
+                                    src={Express}
+                                    alt=""
+                                />
+                            </div>
+                            <div>
+                                <img
+                                    className="logo  w-24 h-24  rounded-xl object-cover "
+                                    src={Mongo}
+                                    alt=""
+                                />
+                            </div>
+                            <div>
+                                <img
+                                    className="logo  w-24 h-24  rounded-xl object-cover "
+                                    src={C}
+                                    alt=""
+                                />
+                            </div>
+                            <div>
+                                <img
+                                    className="logo w-24 h-24 rounded-xl object-cover"
+                                    src={Cpp}
+                                    alt=""
+                                />
+                            </div>
+                            <div>
+                                <img
+                                    className="logo  w-24 h-24  rounded-xl object-cover"
+                                    src={Python}
+                                    alt=""
+                                />
+                            </div>
+                            <div>
+                                <img
+                                    className="logo  w-24 h-24  rounded-xl object-cover "
+                                    src={Vscode}
+                                    alt=""
+                                />
+                            </div>
+                            <div>
+                                <img
+                                    className="logo  w-24 h-24  rounded-xl object-cover "
+                                    src={Java}
+                                    alt=""
+                                />
+                            </div>
+                        </div>
                     </div>
-                    <div >
-                      <img className='logo  w-24 h-24  rounded-xl object-cover ' src={Css} alt="" />
-                    </div>
-                    <div >
-                    <img className='logo  w-24 h-24  rounded-xl object-cover ' src={JS} alt="" />
-                  </div>
-                    <div >
-                    
-                      <img className='logo  w-24 h-24  rounded-xl object-cover ' src={ReactLogo} alt="" />
-                    </div>
-                    <div >
-                      <img className='logo  w-24 h-24  rounded-xl object-cover ' src={Node} alt="" />
-                    </div>
-                    <div >
-                      <img className='logo  w-24 h-24  rounded-xl object-cover ' src={Express} alt="" />
-                    </div>
-                    <div >
-                      <img className='logo  w-24 h-24  rounded-xl object-cover ' src={Mongo} alt="" />
-                    </div>
-                    <div >
-                    <img className='logo  w-24 h-24  rounded-xl object-cover ' src={C} alt="" />
-                    </div>
-                    <div >
-                      <img className='logo w-24 h-24 rounded-xl object-cover' src={Cpp} alt="" />
-                    </div>
-                    <div >
-                      <img className='logo  w-24 h-24  rounded-xl object-cover' src={Python} alt="" />
-                      </div>
-                      <div >
-                      <img className='logo  w-24 h-24  rounded-xl object-cover ' src={Vscode} alt="" />
-                      </div>
-                    <div >
-                      <img className='logo  w-24 h-24  rounded-xl object-cover ' src={Java} alt="" />
-                    </div>
-                  </div>  
                 </div>
-            </div>
-            <div className="right-container md:w-1/2 w-96 p-5">
-                
-      
-                <p className="poppins-regular mt-2 text-sm md:text-lg text-justify">
-                    I am currently in my final year of Computer Engineering. I
-                    am very passionate about technology, programming and
-                    development. I have done Programming in C, C++ and Java.
-                    Currently using C++ for DSA and Problem Solving. Apart from
-                    coding web development has been a part of my interest. I
-                    have made projects in Javascript, React, Node and Express.
-                </p>
-                <p className="poppins-regular mt-2 text-sm md:text-lg text-justify">
-                    I love to explore things & love to implement them in
-                    real-life problems. I'm currently trying to learn various
-                    technologies and will definitely master them one day.
-                </p>
-    
-                <div className="flex justify-center md:justify-normal mt-4 gap-3 md:gap-5">
-                    <Link
-                        to="https://www.linkedin.com/in/sdevrajkumarpatel/"
-                        className="text-xl md:text-3xl p-3 bg-white text-blue-700 rounded-full drop-shadow-md hover:bg-black hover:text-white"
-                    >
-                        <FaLinkedin />
-                    </Link>
-                    <Link 
-                    to="https://github.com/vrajkmrpatel"
-                    className="text-xl md:text-3xl p-3 bg-white text-black rounded-full drop-shadow-md hover:bg-black hover:text-white">
-                        <FaGithub />
-                    </Link>
-                    <Link 
-                    to="https://leetcode.com/Vrajkmr/"
-                    className="text-xl md:text-3xl p-3 bg-white text-black rounded-full drop-shadow-md hover:bg-black hover:text-white">
-                        <SiLeetcode />
-                    </Link>
-                    <Link 
-                    to="https://auth.geeksforgeeks.org/user/vrajpatel31"
-                    className="text-xl md:text-3xl p-3 bg-white text-green-500 rounded-full drop-shadow-md hover:bg-black hover:text-white">
-                        <SiGeeksforgeeks />
-                    </Link>
-                    <Link 
-                    to="https://twitter.com/ilovecompiler"
-                    className="text-xl md:text-3xl p-3 bg-white text-black rounded-full drop-shadow-md hover:bg-black hover:text-white">
-                        <FaSquareXTwitter />
-                    </Link>
+                <div className="right-container md:w-1/2 w-96 p-5">
+                    <p className="poppins-regular mt-2 text-sm md:text-lg text-justify">
+                        I am currently in my final year of Computer Engineering.
+                        I am very passionate about technology, programming and
+                        development. I have done Programming in C, C++ and Java.
+                        Currently using C++ for DSA and Problem Solving. Apart
+                        from coding web development has been a part of my
+                        interest. I have made projects in Javascript, React,
+                        Node and Express.
+                    </p>
+                    <p className="poppins-regular mt-2 text-sm md:text-lg text-justify">
+                        I love to explore things & love to implement them in
+                        real-life problems. I'm currently trying to learn
+                        various technologies and will definitely master them one
+                        day.
+                    </p>
+
+                    <div className="flex justify-center md:justify-normal mt-4 gap-3 md:gap-5">
+                        <Link
+                            to="https://www.linkedin.com/in/sdevrajkumarpatel/"
+                            className="text-xl md:text-3xl p-3 bg-white text-blue-700 rounded-full drop-shadow-md hover:bg-black hover:text-white"
+                        >
+                            <FaLinkedin />
+                        </Link>
+                        <Link
+                            to="https://github.com/vrajkmrpatel"
+                            className="text-xl md:text-3xl p-3 bg-white text-black rounded-full drop-shadow-md hover:bg-black hover:text-white"
+                        >
+                            <FaGithub />
+                        </Link>
+                        <Link
+                            to="https://leetcode.com/Vrajkmr/"
+                            className="text-xl md:text-3xl p-3 bg-white text-black rounded-full drop-shadow-md hover:bg-black hover:text-white"
+                        >
+                            <SiLeetcode />
+                        </Link>
+                        <Link
+                            to="https://auth.geeksforgeeks.org/user/vrajpatel31"
+                            className="text-xl md:text-3xl p-3 bg-white text-green-500 rounded-full drop-shadow-md hover:bg-black hover:text-white"
+                        >
+                            <SiGeeksforgeeks />
+                        </Link>
+                        <Link
+                            to="https://twitter.com/ilovecompiler"
+                            className="text-xl md:text-3xl p-3 bg-white text-black rounded-full drop-shadow-md hover:bg-black hover:text-white"
+                        >
+                            <FaSquareXTwitter />
+                        </Link>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-  )
-}
+    );
+};
 
-export default About
+export default About;
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,23 +1,155 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Contact = () => {
+    const [result, setResult] = useState("");
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        setResult("Sending....");
+        const formData = new FormData(event.target);
+
+        formData.append("access_key", "2be010e3-70e0-4a00-a72e-a370573b8fe3");
+
+        const response = await fetch("https://api.web3forms.com/submit", {
+            method: "POST",
+            body: formData,
+        });
+
+        const data = await response.json();
+
+        if (data.success) {
+            setResult("Form Submitted Successfully");
+            event.target.reset();
+        } else {
+            console.log("Error", data);
+            setResult(data.message);
+        }
+    };
+
     return (
         <div className="container mx-auto w-full mt-5 ">
             <div className="flex flex-col justify-center items-center">
                 <div className="poppins-bold mb-4 tracking-tight text-3xl sm:text-5xl text-center">
                     Get in Touch
                 </div>
-               
-                   <form 
-                   className="w-[80%] flex justify-center items-center gap-4 flex-col">
-                        <input type="text" className="input input-bordered w-full" placeholder="Your Name"/>
-                        <input type="email" className="input input-bordered w-full" placeholder="Your Email"/>
-                        <textarea className="textarea textarea-bordered w-full h-48 " placeholder="Enter Message..."></textarea>
-                        <button type="submit" className="btn btn-primary rounded-full w-full">Send Message</button>
-                   </form>
-                   
+
+                <div className="w-full container mx-auto flex justify-center items-center flex-wrap mt-5">
+                    <div className="left-container md:w-1/2 w-96 p-5 ">
+                        <div class="mb-6 space-y-8 md:mt-8">
+                            <p class="flex items-start -mx-2">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    class="w-6 h-6 mx-2 text-blue-500 dark:text-blue-400"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    stroke="currentColor"
+                                    stroke-width="2"
+                                >
+                                    <path
+                                        stroke-linecap="round"
+                                        stroke-linejoin="round"
+                                        d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"
+                                    />
+                                    <path
+                                        stroke-linecap="round"
+                                        stroke-linejoin="round"
+                                        d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"
+                                    />
+                                </svg>
+
+                                <span class="mx-2  truncate w-72 dark:text-gray-400">
+                                    Vadodara, Gujarat, India 390009
+                                </span>
+                            </p>
+
+                            <p class="flex items-start -mx-2">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    class="w-6 h-6 mx-2 text-blue-500 dark:text-blue-400"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    stroke="currentColor"
+                                    stroke-width="2"
+                                >
+                                    <path
+                                        stroke-linecap="round"
+                                        stroke-linejoin="round"
+                                        d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"
+                                    />
+                                </svg>
+
+                                <span class="mx-2 truncate w-72 dark:text-gray-400">
+                                    +91 9512331224
+                                </span>
+                            </p>
+
+                            <p class="flex items-start -mx-2">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    class="w-6 h-6 mx-2 text-blue-500 dark:text-blue-400"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    stroke="currentColor"
+                                    stroke-width="2"
+                                >
+                                    <path
+                                        stroke-linecap="round"
+                                        stroke-linejoin="round"
+                                        d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
+                                    />
+                                </svg>
+
+                                <span class="mx-2  truncate w-72 dark:text-gray-400">
+                                    31vrajpatel@gmail.com
+                                </span>
+                            </p>
+                        </div>
+                        <div class="overflow-hidden rounded-lg lg:col-span-2 h-96">
+                            <iframe
+                                width="100%"
+                                height="100%"
+                                frameborder="0"
+                                title="map"
+                                marginheight="0"
+                                marginwidth="0"
+                                src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d62732.38348851792!2d73.19419789199077!3d22.295840357599445!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sin!4v1714757061047!5m2!1sen!2sin"
+                            ></iframe>
+                        </div>
+                    </div>
+                    <div className="right-container md:w-1/2 w-96 p-5 flex justify-center flex-col items-center">
+                        <form
+                            onSubmit={onSubmit}
+                            className="w-[80%] flex justify-center items-center gap-4 flex-col"
+                        >
+                        <input type="hidden" name="access_key" value="2be010e3-70e0-4a00-a72e-a370573b8fe3"></input>
+                            <input
+                                type="text"
+                                className="input input-bordered w-full"
+                                placeholder="Your Name"
+                                required
+                            />
+                            <input
+                                type="email"
+                                className="input input-bordered w-full"
+                                placeholder="Your Email"
+                                required
+                            />
+                            <textarea
+                                className="textarea textarea-bordered w-full h-48 "
+                                placeholder="Enter Message..."
+                                required
+                            ></textarea>
+                            <button
+                                type="submit"
+                                className="btn btn-primary rounded-full w-full"
+                            >
+                                Send Message
+                            </button>
+                        </form>
+                        <span>{result}</span>
+                    </div>
                 </div>
-        
+            </div>
         </div>
     );
 };
