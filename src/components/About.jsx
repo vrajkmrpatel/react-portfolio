97c1707ce70e2c4baf0e09d60f76bd439658262e
Vrajkumar Patel <31vrajpatel@gmail.com>
2024-09-23 23:17:43 +0530

    update about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -21,9 +21,9 @@ import { Link } from "react-router-dom";
 const About = () => {
     return (
         <div className="container mx-auto w-full  mt-5 ">
-        <div className="poppins-bold text-primary mb-4 tracking-tight text-3xl sm:text-5xl text-center">
-        About
-    </div>
+            <div className="poppins-bold text-primary mb-4 tracking-tight text-3xl sm:text-5xl text-center">
+                About
+            </div>
             <div className="w-full container mx-auto flex justify-center items-center flex-wrap-reverse mt-5">
                 <div className="left-container md:w-1/2 w-96 p-5 ">
                     <div className="technologies-container">
@@ -120,19 +120,27 @@ const About = () => {
                 </div>
                 <div className="right-container md:w-1/2 w-96 p-5">
                     <p className="poppins-regular mt-2 text-sm md:text-lg text-justify">
-                        I am currently in my final year of Computer Engineering.
-                        I am very passionate about technology, programming and
-                        development. I have done Programming in C, C++ and Java.
-                        Currently using C++ for DSA and Problem Solving. Apart
-                        from coding web development has been a part of my
-                        interest. I have made projects in Javascript, React,
-                        Node and Express.
+                        I am currently pursuing my MTech in Information and
+                        Communication Technology with a specialization in
+                        Software Systems at Dhirubhai Ambani Institute Of
+                        Information and Communication Technology. I have
+                        completed my BTech in Computer Science Engineering from
+                        Government Engineering College Gandhinagar. I have
+                        successfully qualified for the GATE 2024 exams, which
+                        enabled me to secure admission into DAIICT's esteemed
+                        MTech ICT program. My passion lies in technology,
+                        programming, and development. I have experience in C,
+                        C++, and Java, and I primarily use C++ for
+                        problem-solving. Apart from coding, Web development has
+                        been a part of my interest, which leading me to create
+                        projects using JavaScript and React.
                     </p>
                     <p className="poppins-regular mt-2 text-sm md:text-lg text-justify">
-                        I love to explore things & love to implement them in
-                        real-life problems. I'm currently trying to learn
-                        various technologies and will definitely master them one
-                        day.
+                        I am always eager to explore new technologies and apply
+                        them to solve real-world problems. My current academic
+                        journey is focused on expanding my knowledge and
+                        expertise,and I am committed to mastering the
+                        technologies I engage with.
                     </p>
 
                     <div className="flex justify-center md:justify-normal mt-4 gap-3 md:gap-5">
